71e128e4d9939c8cab48fa15722e752907343069
aa-teye <alexteyeametepey@gmail.com>
2026-05-30 16:23:47 +0000

    Add churchData.js for faster loading

---
+++ churchData.js
@@ -0,0 +1,227 @@
+// ONC Church Data - Generated
+// Contains static shepherd and member data
+// Last updated: May 2026
+
+window.CHURCH_DATA = {
+  spsShepherds: [
+  {
+    "name": "Frank Armah",
+    "contact": "0558150432",
+    "zone": "Zone 1"
+  },
+  {
+    "name": "Bright Mamene",
+    "contact": "0546024584",
+    "zone": "Zone 1"
+  },
+  {
+    "name": "Christiana Konzondong",
+    "contact": "0549722727",
+    "zone": "Zone 1"
+  },
+  {
+    "name": "Gloria Owusu Ansah",
+    "contact": "0598427368",
+    "zone": "Zone 1"
+  },
+  {
+    "name": "Linda Neequaye",
+    "contact": "0204752124",
+    "zone": "Zone 2"
+  },
+  {
+    "name": "Getrude Abena Owusu Achiaa",
+    "contact": "0242384662",
+    "zone": "Zone 2"
+  },
+  {
+    "name": "Gloria Lartey",
+    "contact": "0532458862",
+    "zone": "Zone 2"
+  },
+  {
+    "name": "Priscilla Sedi Anatsui",
+    "contact": "0552365696",
+    "zone": "Zone 2"
+  },
+  {
+    "name": "Abigail Akakpo",
+    "contact": "0246807808",
+    "zone": "Zone 3"
+  },
+  {
+    "name": "LP. Sophia Korkor",
+    "contact": "0236929939",
+    "zone": "Zone 3"
+  },
+  {
+    "name": "Wisdom Akakpo",
+    "contact": "0246461508",
+    "zone": "Zone 3"
+  },
+  {
+    "name": "Cyril Amevor",
+    "contact": "0246038534",
+    "zone": "Zone 3"
+  },
+  {
+    "name": "Mr. Ebenezer Okronipa",
+    "contact": "0243310124",
+    "zone": "Zone 4"
+  },
+  {
+    "name": "Solomon Aziakah",
+    "contact": "0245775546",
+    "zone": "Zone 4"
+  },
+  {
+    "name": "Deborah Otumfuor",
+    "contact": "0203219321",
+    "zone": "Zone 4"
+  },
+  {
+    "name": "Patience Addo",
+    "contact": "0247816836",
+    "zone": "Zone 4"
+  },
+  {
+    "name": "Benjamin Armah Agyeman",
+    "contact": "0204027587",
+    "zone": "Zone 5"
+  },
+  {
+    "name": "Martha Asinyo",
+    "contact": "0542365036",
+    "zone": "Zone 5"
+  },
+  {
+    "name": "Shine Asinyo",
+    "contact": "0594742093",
+    "zone": "Zone 5"
+  },
+  {
+    "name": "Patience Mensah",
+    "contact": "0208003701",
+    "zone": "Zone 5"
+  },
+  {
+    "name": "Ruth Yeboah",
+    "contact": "0242972177",
+    "zone": "Zone 6"
+  },
+  {
+    "name": "Sharon Tricia Amanu",
+    "contact": "0246505610",
+    "zone": "Zone 6"
+  }
+],
+  mcShepherds: [
+  {
+    "name": "Michael Ayisia",
+    "microchurch": "Tesano 1",
+    "contact": "0548201673"
+  },
+  {
+    "name": "Michael Ameyaw (Tesano 2)",
+    "microchurch": "Tesano 2",
+    "contact": "0240727654"
+  },
+  {
+    "name": "Michael Ameyaw (Tesano 3)",
+    "microchurch": "Tesano 3",
+    "contact": "0240727654"
+  },
+  {
+    "name": "Augustina Selinam Abbah",
+    "microchurch": "Tesano 4",
+    "contact": "0537247314"
+  },
+  {
+    "name": "Blessing Solomon",
+    "microchurch": "Flat Top",
+    "contact": "0543359364"
+  },
+  {
+    "name": "Lp Esther Okronipa",
+    "microchurch": "Official Town",
+    "contact": "0542111091"
+  },
+  {
+    "name": "Benjamin Senyasu",
+    "microchurch": "Police Depot",
+    "contact": "0257173153"
+  },
+  {
+    "name": "Emmanuel Mensah",
+    "microchurch": "Bubiashie",
+    "contact": "0530291764"
+  },
+  {
+    "name": "Eric Appiah",
+    "microchurch": "North Kaneshie Online",
+    "contact": "0556319369"
+  },
+  {
+    "name": "Alex Teye Ametepey",
+    "microchurch": "UG Legon",
+    "contact": "0549044977"
+  },
+  {
+    "name": "Rev Dr Ebenezer Okronipa (Afropa 2)",
+    "microchurch": "Afropa 2",
+    "contact": "0200994446"
+  },
+  {
+    "name": "Rev Dr Ebenezer Okronipa (Manasseh)",
+    "microchurch": "Manasseh",
+    "contact": "0241503359"
+  },
+  {
+    "name": "Rosemond Quaicoe",
+    "microchurch": "Nyamekye",
+    "contact": "0556413103"
+  },
+  {
+    "name": "Henry Mensah",
+    "microchurch": "Gbawe",
+    "contact": "0202009492"
+  },
+  {
+    "name": "Abigail Ankomah",
+    "microchurch": "Ablekuma-Agape",
+    "contact": "0549362549"
+  },
+  {
+    "name": "Alice Ametepey",
+    "microchurch": "Darkuman 3",
+    "contact": "0594081707"
+  },
+  {
+    "name": "Samuel Opare",
+    "microchurch": "New Fadama 2",
+    "contact": "0243545811"
+  },
+  {
+    "name": "Esther Doku",
+    "microchurch": "New-Fadama 1",
+    "contact": "0555797224"
+  },
+  {
+    "name": "Samuel Oteng",
+    "microchurch": "Nsakina",
+    "contact": "0558756473"
+  }
+],
+  zones: [
+    { name: "Zone 1", shepherds: ["Frank Armah", "Bright Mamene", "Christiana Konzondong", "Gloria Owusu Ansah"] },
+    { name: "Zone 2", shepherds: ["Linda Neequaye", "Getrude Abena Owusu Achiaa", "Gloria Lartey", "Priscilla Sedi Anatsui"] },
+    { name: "Zone 3", shepherds: ["Abigail Akakpo", "LP. Sophia Korkor", "Wisdom Akakpo", "Cyril Amevor"] },
+    { name: "Zone 4", shepherds: ["Mr. Ebenezer Okronipa", "Solomon Aziakah", "Deborah Otumfuor", "Patience Addo"] },
+    { name: "Zone 5", shepherds: ["Benjamin Armah Agyeman", "Martha Asinyo", "Shine Asinyo", "Patience Mensah"] },
+    { name: "Zone 6", shepherds: ["Ruth Yeboah", "Sharon Tricia Amanu"] }
+  ]
+};
+
+console.log('✅ Church data loaded:', 
+  window.CHURCH_DATA.spsShepherds.length, 'SPS shepherds,',
+  window.CHURCH_DATA.mcShepherds.length, 'MC shepherds');
