72316d70fcb3da88f41dd22d0169653af2beb87e
aa-teye <alexteyeametepey@gmail.com>
2026-05-31 00:05:44 +0000

    Update churchData.js with 28 SPS shepherds and members

--- churchData.js
+++ churchData.js
@@ -1,227 +1,602 @@
-// ONC Church Data - Generated
-// Contains static shepherd and member data
+// ONC Church Data - Generated from SPS Assigned List Batch 1
+// SPS Shepherds: 28 | MC Shepherds: 19 | Total Members: 83
 // Last updated: May 2026
 
 window.CHURCH_DATA = {
   spsShepherds: [
-  {
-    "name": "Frank Armah",
-    "contact": "0558150432",
-    "zone": "Zone 1"
-  },
-  {
-    "name": "Bright Mamene",
-    "contact": "0546024584",
-    "zone": "Zone 1"
-  },
-  {
-    "name": "Christiana Konzondong",
-    "contact": "0549722727",
-    "zone": "Zone 1"
-  },
-  {
-    "name": "Gloria Owusu Ansah",
-    "contact": "0598427368",
-    "zone": "Zone 1"
-  },
-  {
-    "name": "Linda Neequaye",
-    "contact": "0204752124",
-    "zone": "Zone 2"
-  },
-  {
-    "name": "Getrude Abena Owusu Achiaa",
-    "contact": "0242384662",
-    "zone": "Zone 2"
-  },
-  {
-    "name": "Gloria Lartey",
-    "contact": "0532458862",
-    "zone": "Zone 2"
-  },
-  {
-    "name": "Priscilla Sedi Anatsui",
-    "contact": "0552365696",
-    "zone": "Zone 2"
-  },
-  {
-    "name": "Abigail Akakpo",
-    "contact": "0246807808",
-    "zone": "Zone 3"
-  },
-  {
-    "name": "LP. Sophia Korkor",
-    "contact": "0236929939",
-    "zone": "Zone 3"
-  },
-  {
-    "name": "Wisdom Akakpo",
-    "contact": "0246461508",
-    "zone": "Zone 3"
-  },
-  {
-    "name": "Cyril Amevor",
-    "contact": "0246038534",
-    "zone": "Zone 3"
-  },
-  {
-    "name": "Mr. Ebenezer Okronipa",
-    "contact": "0243310124",
-    "zone": "Zone 4"
-  },
-  {
-    "name": "Solomon Aziakah",
-    "contact": "0245775546",
-    "zone": "Zone 4"
-  },
-  {
-    "name": "Deborah Otumfuor",
-    "contact": "0203219321",
-    "zone": "Zone 4"
-  },
-  {
-    "name": "Patience Addo",
-    "contact": "0247816836",
-    "zone": "Zone 4"
-  },
-  {
-    "name": "Benjamin Armah Agyeman",
-    "contact": "0204027587",
-    "zone": "Zone 5"
-  },
-  {
-    "name": "Martha Asinyo",
-    "contact": "0542365036",
-    "zone": "Zone 5"
-  },
-  {
-    "name": "Shine Asinyo",
-    "contact": "0594742093",
-    "zone": "Zone 5"
-  },
-  {
-    "name": "Patience Mensah",
-    "contact": "0208003701",
-    "zone": "Zone 5"
-  },
-  {
-    "name": "Ruth Yeboah",
-    "contact": "0242972177",
-    "zone": "Zone 6"
-  },
-  {
-    "name": "Sharon Tricia Amanu",
-    "contact": "0246505610",
-    "zone": "Zone 6"
-  }
-],
+    {
+      "name": "Frank Armah",
+      "contact": "0558150432",
+      "zone": "Zone 1",
+      "members": [
+        {
+          "name": "Peace Amoah",
+          "contact": "0576753741"
+        },
+        {
+          "name": "Christiana Adjei",
+          "contact": "0559357150"
+        }
+      ]
+    },
+    {
+      "name": "Bright Mamene",
+      "contact": "0546024584",
+      "zone": "Zone 1",
+      "members": [
+        {
+          "name": "Majeed Saleem",
+          "contact": "0553250246"
+        },
+        {
+          "name": "Isaac Yeboah",
+          "contact": "0263153156"
+        }
+      ]
+    },
+    {
+      "name": "Christiana Konzondong",
+      "contact": "0549722727",
+      "zone": "Zone 1",
+      "members": [
+        {
+          "name": "Bernice Mensah",
+          "contact": "0555906005"
+        },
+        {
+          "name": "Isabel Opoku",
+          "contact": "0593992496"
+        }
+      ]
+    },
+    {
+      "name": "Gloria Owusu Ansah",
+      "contact": "0598427368",
+      "zone": "Zone 1",
+      "members": [
+        {
+          "name": "David Kudadze",
+          "contact": "0558212556"
+        },
+        {
+          "name": "Rebecca Opoku",
+          "contact": "0544581848"
+        }
+      ]
+    },
+    {
+      "name": "Linda Neequaye",
+      "contact": "0204752124",
+      "zone": "Zone 2",
+      "members": [
+        {
+          "name": "Mary Seke",
+          "contact": "0540769216"
+        },
+        {
+          "name": "Jedidiah Wiafe",
+          "contact": "0530280871"
+        }
+      ]
+    },
+    {
+      "name": "Getrude Abena Owusu Achiaa",
+      "contact": "0242384662",
+      "zone": "Zone 2",
+      "members": [
+        {
+          "name": "Williams Asomaning Ameyaw",
+          "contact": "0531333868"
+        },
+        {
+          "name": "Mr. Abraham Can-Tamakloe",
+          "contact": "0542880155"
+        }
+      ]
+    },
+    {
+      "name": "Gloria Lartey",
+      "contact": "0532458862",
+      "zone": "Zone 2",
+      "members": [
+        {
+          "name": "Eugene Ampadu",
+          "contact": "0595976766"
+        },
+        {
+          "name": "Grace Sakyibea",
+          "contact": "0591210506"
+        }
+      ]
+    },
+    {
+      "name": "Priscilla Sedi Anatsui",
+      "contact": "0552365696",
+      "zone": "Zone 2",
+      "members": [
+        {
+          "name": "Sunday Amuzu",
+          "contact": "0591388442"
+        },
+        {
+          "name": "Promise Bamflo",
+          "contact": "0534004294"
+        }
+      ]
+    },
+    {
+      "name": "Abigail Akakpo",
+      "contact": "0246807808",
+      "zone": "Zone 3",
+      "members": [
+        {
+          "name": "Deborah Quaye",
+          "contact": "0550414130"
+        },
+        {
+          "name": "Veronica Bemponmaa Boateng",
+          "contact": "0544253677"
+        }
+      ]
+    },
+    {
+      "name": "LP. Sophia Korkor",
+      "contact": "0236929939",
+      "zone": "Zone 3",
+      "members": [
+        {
+          "name": "Janelle Kekeli Dartey",
+          "contact": "0204752124"
+        },
+        {
+          "name": "Emmanuel Kwaku Amon",
+          "contact": "0249261062"
+        },
+        {
+          "name": "Francis Kweku Opoku Ivan",
+          "contact": "0540801882"
+        }
+      ]
+    },
+    {
+      "name": "Wisdom Akakpo",
+      "contact": "0246461508",
+      "zone": "Zone 3",
+      "members": [
+        {
+          "name": "Dorcas Janssen",
+          "contact": "0542506063"
+        },
+        {
+          "name": "Lawrence Gbeku",
+          "contact": "0504729233"
+        }
+      ]
+    },
+    {
+      "name": "Cyril Amevor",
+      "contact": "0246038534",
+      "zone": "Zone 3",
+      "members": [
+        {
+          "name": "Princess Gyamfuaa",
+          "contact": "0539487516"
+        },
+        {
+          "name": "Philip Boateng",
+          "contact": "0544361936"
+        }
+      ]
+    },
+    {
+      "name": "Solomon Aziakah",
+      "contact": "0245775546",
+      "zone": "Zone 4",
+      "members": [
+        {
+          "name": "Mrs. Elizabeth Agbedo",
+          "contact": "0574745487"
+        },
+        {
+          "name": "Shirley Williams",
+          "contact": "0242927554"
+        }
+      ]
+    },
+    {
+      "name": "Deborah Otumfuor",
+      "contact": "0203219321",
+      "zone": "Zone 4",
+      "members": [
+        {
+          "name": "George Sarfo Mensah",
+          "contact": "0570002526"
+        },
+        {
+          "name": "Madam Paulina Mansa Atiase",
+          "contact": "0246634036"
+        }
+      ]
+    },
+    {
+      "name": "Patience Addo",
+      "contact": "0247816836",
+      "zone": "Zone 4",
+      "members": [
+        {
+          "name": "Janet Oye",
+          "contact": "0232419267"
+        },
+        {
+          "name": "Maame Efua Owusu Boateng",
+          "contact": "0536287869"
+        }
+      ]
+    },
+    {
+      "name": "Benjamin Armah Agyeman",
+      "contact": "0204027587",
+      "zone": "Zone 5",
+      "members": [
+        {
+          "name": "Sylvester Effah",
+          "contact": "0554418419"
+        },
+        {
+          "name": "Ruth Otoo",
+          "contact": "0593963893"
+        }
+      ]
+    },
+    {
+      "name": "Martha Asinyo",
+      "contact": "0542365036",
+      "zone": "Zone 5",
+      "members": [
+        {
+          "name": "Stanley Boateng",
+          "contact": "0552732231"
+        },
+        {
+          "name": "Abigail Asantewaa-Boadu",
+          "contact": "0244367411"
+        }
+      ]
+    },
+    {
+      "name": "Shine Asinyo",
+      "contact": "0594742093",
+      "zone": "Zone 5",
+      "members": [
+        {
+          "name": "Rosemary Obeng Appiah",
+          "contact": "0598568058"
+        },
+        {
+          "name": "Lewis Nana Kojo",
+          "contact": "0536713383"
+        }
+      ]
+    },
+    {
+      "name": "Patience Mensah",
+      "contact": "0208003701",
+      "zone": "Zone 5",
+      "members": [
+        {
+          "name": "Florence Ablordey",
+          "contact": "0205407783"
+        },
+        {
+          "name": "Bernice Agyapong Quaye",
+          "contact": "0552758501"
+        },
+        {
+          "name": "Lucy Yaa B Agyemang",
+          "contact": "0540801882"
+        }
+      ]
+    },
+    {
+      "name": "Ruth Yeboah",
+      "contact": "0242972177",
+      "zone": "Zone 6",
+      "members": [
+        {
+          "name": "Selina Ofeibea",
+          "contact": "0537619776"
+        },
+        {
+          "name": "Isaac Oblitey",
+          "contact": "0538322909"
+        },
+        {
+          "name": "Justina Tetteh",
+          "contact": "0544766416"
+        }
+      ]
+    },
+    {
+      "name": "Sharon Tricia Amanu",
+      "contact": "0246505610",
+      "zone": "Zone 6",
+      "members": [
+        {
+          "name": "Joseph Atsu",
+          "contact": "0594593966"
+        },
+        {
+          "name": "Michael Kwabena Agyapong Effah",
+          "contact": "0541665108"
+        }
+      ]
+    },
+    {
+      "name": "Mrs Petrina Gyane",
+      "contact": "",
+      "zone": "Zone 6",
+      "members": [
+        {
+          "name": "Kevin Nyarko",
+          "contact": "0542082093"
+        },
+        {
+          "name": "Agnes Amoako",
+          "contact": "0535067474"
+        }
+      ]
+    },
+    {
+      "name": "Samuel Tortor",
+      "contact": "",
+      "zone": "Zone 6",
+      "members": [
+        {
+          "name": "Danis Agbormava",
+          "contact": "0539648405"
+        },
+        {
+          "name": "Diana Ameyaw",
+          "contact": "0249670978"
+        }
+      ]
+    },
+    {
+      "name": "Loretta Owusu",
+      "contact": "",
+      "zone": "Zone 7",
+      "members": [
+        {
+          "name": "Gerald Opoku",
+          "contact": "0536122100"
+        }
+      ]
+    },
+    {
+      "name": "Melchizedek Ofori",
+      "contact": "",
+      "zone": "Zone 7",
+      "members": [
+        {
+          "name": "Kingsley Kobina Aduful",
+          "contact": "0598951791"
+        },
+        {
+          "name": "Titus Quaye",
+          "contact": "0241631779"
+        }
+      ]
+    },
+    {
+      "name": "Bernice Can- Tamakloe",
+      "contact": "",
+      "zone": "Zone 7",
+      "members": [
+        {
+          "name": "Emmanuel Nii-Lantei",
+          "contact": "0599962519"
+        },
+        {
+          "name": "Rebecca Donkor",
+          "contact": "0530217959"
+        }
+      ]
+    },
+    {
+      "name": "Ruth Dadzie",
+      "contact": "",
+      "zone": "Zone 7",
+      "members": [
+        {
+          "name": "Samuel Adama",
+          "contact": "0208759771"
+        },
+        {
+          "name": "Hannah Akuffo France",
+          "contact": "0240613887"
+        }
+      ]
+    },
+    {
+      "name": "Anita Asamoah",
+      "contact": "",
+      "zone": "Zone 7",
+      "members": [
+        {
+          "name": "Joseph Asmah",
+          "contact": "0549945636"
+        },
+        {
+          "name": "Jeremiah Asante",
+          "contact": "0595580993"
+        }
+      ]
+    }
+  ],
   mcShepherds: [
-  {
-    "name": "Michael Ayisia",
-    "microchurch": "Tesano 1",
-    "contact": "0548201673"
-  },
-  {
-    "name": "Michael Ameyaw (Tesano 2)",
-    "microchurch": "Tesano 2",
-    "contact": "0240727654"
-  },
-  {
-    "name": "Michael Ameyaw (Tesano 3)",
-    "microchurch": "Tesano 3",
-    "contact": "0240727654"
-  },
-  {
-    "name": "Augustina Selinam Abbah",
-    "microchurch": "Tesano 4",
-    "contact": "0537247314"
-  },
-  {
-    "name": "Blessing Solomon",
-    "microchurch": "Flat Top",
-    "contact": "0543359364"
-  },
-  {
-    "name": "Lp Esther Okronipa",
-    "microchurch": "Official Town",
-    "contact": "0542111091"
-  },
-  {
-    "name": "Benjamin Senyasu",
-    "microchurch": "Police Depot",
-    "contact": "0257173153"
-  },
-  {
-    "name": "Emmanuel Mensah",
-    "microchurch": "Bubiashie",
-    "contact": "0530291764"
-  },
-  {
-    "name": "Eric Appiah",
-    "microchurch": "North Kaneshie Online",
-    "contact": "0556319369"
-  },
-  {
-    "name": "Alex Teye Ametepey",
-    "microchurch": "UG Legon",
-    "contact": "0549044977"
-  },
-  {
-    "name": "Rev Dr Ebenezer Okronipa (Afropa 2)",
-    "microchurch": "Afropa 2",
-    "contact": "0200994446"
-  },
-  {
-    "name": "Rev Dr Ebenezer Okronipa (Manasseh)",
-    "microchurch": "Manasseh",
-    "contact": "0241503359"
-  },
-  {
-    "name": "Rosemond Quaicoe",
-    "microchurch": "Nyamekye",
-    "contact": "0556413103"
-  },
-  {
-    "name": "Henry Mensah",
-    "microchurch": "Gbawe",
-    "contact": "0202009492"
-  },
-  {
-    "name": "Abigail Ankomah",
-    "microchurch": "Ablekuma-Agape",
-    "contact": "0549362549"
-  },
-  {
-    "name": "Alice Ametepey",
-    "microchurch": "Darkuman 3",
-    "contact": "0594081707"
-  },
-  {
-    "name": "Samuel Opare",
-    "microchurch": "New Fadama 2",
-    "contact": "0243545811"
-  },
-  {
-    "name": "Esther Doku",
-    "microchurch": "New-Fadama 1",
-    "contact": "0555797224"
-  },
-  {
-    "name": "Samuel Oteng",
-    "microchurch": "Nsakina",
-    "contact": "0558756473"
-  }
-],
+    {
+      "name": "Michael Ayisia",
+      "microchurch": "Tesano 1",
+      "contact": "0548201673"
+    },
+    {
+      "name": "Michael Ameyaw (Tesano 2)",
+      "microchurch": "Tesano 2",
+      "contact": "0240727654"
+    },
+    {
+      "name": "Michael Ameyaw (Tesano 3)",
+      "microchurch": "Tesano 3",
+      "contact": "0240727654"
+    },
+    {
+      "name": "Augustina Selinam Abbah",
+      "microchurch": "Tesano 4",
+      "contact": "0537247314"
+    },
+    {
+      "name": "Blessing Solomon",
+      "microchurch": "Flat Top",
+      "contact": "0543359364"
+    },
+    {
+      "name": "Lp Esther Okronipa",
+      "microchurch": "Official Town",
+      "contact": "0542111091"
+    },
+    {
+      "name": "Benjamin Senyasu",
+      "microchurch": "Police Depot",
+      "contact": "0257173153"
+    },
+    {
+      "name": "Emmanuel Mensah",
+      "microchurch": "Bubiashie",
+      "contact": "0530291764"
+    },
+    {
+      "name": "Eric Appiah",
+      "microchurch": "North Kaneshie Online",
+      "contact": "0556319369"
+    },
+    {
+      "name": "Alex Teye Ametepey",
+      "microchurch": "UG Legon",
+      "contact": "0549044977"
+    },
+    {
+      "name": "Rev Dr Ebenezer Okronipa (Afropa 2)",
+      "microchurch": "Afropa 2",
+      "contact": "0200994446"
+    },
+    {
+      "name": "Rev Dr Ebenezer Okronipa (Manasseh)",
+      "microchurch": "Manasseh",
+      "contact": "0241503359"
+    },
+    {
+      "name": "Rosemond Quaicoe",
+      "microchurch": "Nyamekye",
+      "contact": "0556413103"
+    },
+    {
+      "name": "Henry Mensah",
+      "microchurch": "Gbawe",
+      "contact": "0202009492"
+    },
+    {
+      "name": "Abigail Ankomah",
+      "microchurch": "Ablekuma-Agape",
+      "contact": "0549362549"
+    },
+    {
+      "name": "Alice Ametepey",
+      "microchurch": "Darkuman 3",
+      "contact": "0594081707"
+    },
+    {
+      "name": "Samuel Opare",
+      "microchurch": "New Fadama 2",
+      "contact": "0243545811"
+    },
+    {
+      "name": "Esther Doku",
+      "microchurch": "New-Fadama 1",
+      "contact": "0555797224"
+    },
+    {
+      "name": "Samuel Oteng",
+      "microchurch": "Nsakina",
+      "contact": "0558756473"
+    }
+  ],
   zones: [
-    { name: "Zone 1", shepherds: ["Frank Armah", "Bright Mamene", "Christiana Konzondong", "Gloria Owusu Ansah"] },
-    { name: "Zone 2", shepherds: ["Linda Neequaye", "Getrude Abena Owusu Achiaa", "Gloria Lartey", "Priscilla Sedi Anatsui"] },
-    { name: "Zone 3", shepherds: ["Abigail Akakpo", "LP. Sophia Korkor", "Wisdom Akakpo", "Cyril Amevor"] },
-    { name: "Zone 4", shepherds: ["Mr. Ebenezer Okronipa", "Solomon Aziakah", "Deborah Otumfuor", "Patience Addo"] },
-    { name: "Zone 5", shepherds: ["Benjamin Armah Agyeman", "Martha Asinyo", "Shine Asinyo", "Patience Mensah"] },
-    { name: "Zone 6", shepherds: ["Ruth Yeboah", "Sharon Tricia Amanu"] }
+    {
+      "name": "Zone 1",
+      "shepherds": [
+        "Frank Armah",
+        "Bright Mamene",
+        "Christiana Konzondong",
+        "Gloria Owusu Ansah"
+      ]
+    },
+    {
+      "name": "Zone 2",
+      "shepherds": [
+        "Linda Neequaye",
+        "Getrude Abena Owusu Achiaa",
+        "Gloria Lartey",
+        "Priscilla Sedi Anatsui"
+      ]
+    },
+    {
+      "name": "Zone 3",
+      "shepherds": [
+        "Abigail Akakpo",
+        "LP. Sophia Korkor",
+        "Wisdom Akakpo",
+        "Cyril Amevor"
+      ]
+    },
+    {
+      "name": "Zone 4",
+      "shepherds": [
+        "Solomon Aziakah",
+        "Deborah Otumfuor",
+        "Patience Addo"
+      ]
+    },
+    {
+      "name": "Zone 5",
+      "shepherds": [
+        "Benjamin Armah Agyeman",
+        "Martha Asinyo",
+        "Shine Asinyo",
+        "Patience Mensah"
+      ]
+    },
+    {
+      "name": "Zone 6",
+      "shepherds": [
+        "Ruth Yeboah",
+        "Sharon Tricia Amanu",
+        "Mrs Petrina Gyane",
+        "Samuel Tortor"
+      ]
+    },
+    {
+      "name": "Zone 7",
+      "shepherds": [
+        "Loretta Owusu",
+        "Melchizedek Ofori",
+        "Bernice Can- Tamakloe",
+        "Ruth Dadzie",
+        "Anita Asamoah"
+      ]
+    }
   ]
 };
 
-console.log('✅ Church data loaded:', 
+console.log('✅ Church data loaded:',
   window.CHURCH_DATA.spsShepherds.length, 'SPS shepherds,',
   window.CHURCH_DATA.mcShepherds.length, 'MC shepherds');
